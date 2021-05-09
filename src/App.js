fd0fcf9c08eefd6b8491e98f69c54aa073a15eff
malapashish <malapashish10@gmail.com>
2021-05-09 14:16:54 +0530

    Version 1 Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useEffect , useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import './App.css';
+import Movie from './components/Movie';
+
+const MoviesAPI = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8e226ac94d6cb225fcb0652695f029d7&page=1`
+
+const SearchAPI = `https://api.themoviedb.org/3/search/movie?&api_key=8e226ac94d6cb225fcb0652695f029d7&query=`
+
+const App = () => {
+
+    const [ movies , setMovies ] = useState([]);
+    const [ searchTerm , setSearchTerm ] = useState('');
+
+    const getMovies = (API) => {
+        axios
+            .get(API)
+            .then((response) => {
+                setMovies(response.data.results);
+            })
+    }
+
+    useEffect(() => {
+        getMovies(MoviesAPI)
+    }, [])
+
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if(searchTerm){
+        getMovies( SearchAPI + searchTerm )
+        setSearchTerm('');   
+        } 
+    }
+
+    const handleSearchTerm = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    return(
+        <>
+        <header>
+                <form onSubmit = {handleOnSubmit}>
+                <input
+                className = 'search'
+                type = 'search'
+                placeholder = 'Search...'
+                value = {searchTerm}
+                onChange = {handleSearchTerm}
+                />
+                </form>
+            </header>
+        <div className = 'movie-container'>      
+            { movies && movies.map((movie) => <Movie key = {movie.id} {...movie} />) }
+        </div>
+        </>
+    )
 }
 
-export default App;
+export default App
