1b479a1d53a3a31317b2bbca9eb8896172be9831
malapashish <malapashish10@gmail.com>
2021-06-02 05:32:16 +0530

    More dubugging done

--- src/Pages/MovieDetails.js
+++ src/Pages/MovieDetails.js
@@ -56,84 +56,40 @@ const MovieDetails = ({ match : { params : {id} } }) => {
                 <NavLink to = '/series' className = 'nav-link' activeClassName = 'active'>
                     Series
                 </NavLink>
-                {/* <div className = 'search-container'>
-                    <form onSubmit = {handleOnSubmit}>
-                    <input
-                    className = 'search'
-                    type = 'text'
-                    placeholder = 'Search...'
-                    value = {searchTerm}
-                    onChange = {handleSearchTerm}
-                    />
-                    </form>
-                </div> */}
             </nav>
             {getYoutubeLink()}
-            <div>
+            <div className = 'movie-details'>
                 <img src={movieDetails.backdrop_path ?  IMG_API+movieDetails.backdrop_path : 'https://image.shutterstock.com/image-vector/picture-vector-icon-no-image-600w-1350441335.jpg'} alt = 'Backdrop_Images' className = "hero-image" /> 
-            </div>
-            {/* <div className = 'second-section'>
-                <img  src = {movieDetails.poster_path ?  IMG_API + movieDetails.poster_path : 'https://image.shutterstock.com/image-vector/picture-vector-icon-no-image-600w-1350441335.jpg'} alt = 'Poster_Image' className = 'poster-image' />
-                <div className = 'content-container'>
-                    <h3>{movieDetails.title}</h3>
-                    <section className = 'content-section'>
-                        <h4 class="summary-title">Summary:</h4>
-                        <p className = 'summary-content'>
-                            {movieDetails.overview}
-                        </p>
-                    </section> 
-                    <section className = 'stats-section'>
-                        <section className = "release-date"> 
-                            <p>Release Date</p>
-                            <p>{movieDetails.release_date}</p>
+                <div className = 'details-section'>
+                    <img  src = {movieDetails.poster_path ?  IMG_API + movieDetails.poster_path : 'https://image.shutterstock.com/image-vector/picture-vector-icon-no-image-600w-1350441335.jpg'} alt = 'Poster_Image' className = 'poster-image' />
+                    <div className = 'information-section'>
+                        <h3>{movieDetails.title}</h3>
+                        <section className = 'summary-section'>
+                            <h4 className="summary-title">Summary:</h4>
+                            <p className = 'summary-content'>{movieDetails.overview}</p>
                         </section>
-                        <section className = 'voting-average'>
-                            <p>Voting Average</p>
-                            <p className = {`tag ${checkrating(movieDetails.vote_average)}`} >{movieDetails.vote_average}</p>
+                        <section className = 'movie-stats-section'>
+                            <section className = 'movie-release-date'>
+                                <p>Release Date</p>
+                                <p>{movieDetails.release_date}</p>
+                            </section>
+                            <section className = 'movie-voting-average'>
+                                <p>Voting Average</p>
+                                <p className = {`tag ${checkrating(movieDetails.vote_average)}`} >{movieDetails.vote_average}</p>
+                            </section>
+                            <section className = 'movie-trailer'>
+                                <Button
+                                    variant="contained"
+                                    startIcon={<YouTubeIcon />}
+                                    color="secondary"
+                                    target="__blank"
+                                    href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                    Watch the Trailer
+                                </Button>
+                            </section>
                         </section>
-                        <section className = 'see_trailler'>
-                            <Button
-                                variant="contained"
-                                startIcon={<YouTubeIcon />}
-                                color="secondary"
-                                target="__blank"
-                                href={`https://www.youtube.com/watch?v=${video}`}
-                            >
-                                Watch the Trailer
-                            </Button>
-                        </section>
-                    </section>
-                </div>
-            </div> */}
-            <div className = 'details-section'>
-                <img  src = {movieDetails.poster_path ?  IMG_API + movieDetails.poster_path : 'https://image.shutterstock.com/image-vector/picture-vector-icon-no-image-600w-1350441335.jpg'} alt = 'Poster_Image' className = 'poster-image' />
-                <div className = 'information-section'>
-                    <h3>{movieDetails.title}</h3>
-                    <section className = 'summary-section'>
-                        <h4 className="summary-title">Summary:</h4>
-                        <p className = 'summary-content'>{movieDetails.overview}</p>
-                    </section>
-                    <section className = 'movie-stats-section'>
-                        <section className = 'movie-release-date'>
-                            <p>Release Date</p>
-                            <p>{movieDetails.release_date}</p>
-                        </section>
-                        <section className = 'movie-voting-average'>
-                            <p>Voting Average</p>
-                            <p className = {`tag ${checkrating(movieDetails.vote_average)}`} >{movieDetails.vote_average}</p>
-                        </section>
-                        <section className = 'movie-trailer'>
-                            <Button
-                                variant="contained"
-                                startIcon={<YouTubeIcon />}
-                                color="secondary"
-                                target="__blank"
-                                href={`https://www.youtube.com/watch?v=${video}`}
-                            >
-                                Watch the Trailer
-                            </Button>
-                        </section>
-                    </section>
+                    </div>
                 </div>
             </div>
         </> 
