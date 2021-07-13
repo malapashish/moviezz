4b32e7f8909a8b688b032a611e7df99d21bd29a5
malapashish <malapashish10@gmail.com>
2021-07-13 17:48:02 +0530

    Added redux to the Home Page Componenet

--- src/containers/Pages/Home.js
+++ src/containers/Pages/Home.js
@@ -3,110 +3,37 @@ import { connect } from 'react-redux';
 import { fetchData } from '../../actions';
 
 import Cards from '../../components/Cards';
-import CustomPagination from '../../components/Pagination';
-// import LikeMessages from '../../components/LikeMessages'; 
-import { Spinner } from '../../components/Spinner';
-import LikeMessages from '../../components/LikeMessages';
-import db from '../../utilities/firebase';
-import { movieDbAPI , imgAPI } from '../../apis';  
+import CustomPagination from '../../components/Pagination'; 
+import { Spinner } from '../../components/Spinner';  
 
 require('dotenv').config();  
 
 const Home = (props) => {
-    const [ page, setPage ] = useState(1);
-    const [ contentList , setContentList ] = useState([]); 
-    const [ favoriteContent , setFavoriteContent] = useState([]);
-    const [ repeatedLiked , setRepeatedLiked ] = useState(null); 
-    const [ message , setMessage ] = useState(null);
- 
-
-    //fetches the movie and series list also fetches favoured movie and series list
+    const [ page, setPage ] = useState(1);  
     useEffect(() => { 
-        props.fetchTrendList(page);
-        // console.log(props);
-        const getTrendingList = async () => {
-        await movieDbAPI
-                        .get(`/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
-                        .then((response) => {
-                            setContentList(response.data.results);
-                        })
-        }
-        getTrendingList();
-        fetchFavourites();
-        window.scroll(0, 0); 
+        props.fetchTrendList(page); 
+        window.scroll(0, 0);  
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page])
-    
-    //fetches favoured movie and list
-    const fetchFavourites = () => {
-        db
-            .collection('favorites')
-            .onSnapshot((q) => {
-               setFavoriteContent(
-                   q.docs.map((doc) => ({
-                       id : doc.id ,
-                       title : doc.data().title , 
-                        poster_path : doc.data().image_path,
-                       vote_average : doc.data().vote_average
-                   }))
-               )
-           }) 
-    }
-
-    /*onclick handler which check if selected movie or series already added into favoured list or not. 
-    If not added add the movie or list if already added then show a warning saying that the 
-    movie or series already added to the favoured list. 
-    */
-    const handleFavorites = (input) => {  
-        if(favoriteContent.some(content =>  content.title === (input.title || input.name))){
-            console.log(favoriteContent);
-            setRepeatedLiked(true); 
-            setTimeout(() => {
-                setRepeatedLiked(null);
-            } , 500);
-        }else{
-            db
-            .collection('favorites')
-            .add({
-                movieId : input.id,
-                title : input.title || input.name,
-                vote_average : input.vote_average,
-                image_path : imgAPI + input.poster_path,
-                media_type : input.media_type
-            })
-            setMessage('Added to the favorites');
-            setTimeout(() => {
-                setMessage(null);
-            } , 500) 
-        }
-    }
- 
+     
     return(
         <>  
-        <span className = 'page-heading'> 
-            <i className="fas fa-fire"></i>   
-            Trending
-        </span>
-        <div className = 'movie-container'>      
-            { contentList && contentList.map((content) => <Cards key = {content.id} {...content} handleFavorites = {handleFavorites} />) }
-        </div>
-        <LikeMessages message = {message} />
-        {
-            repeatedLiked && (
-                <div className = 'snackbar show'>
-                    Already added in the favorites..
-                </div>
-            )
-        }
-        {console.log('Data : ' , props.trendingList)}
-        <Spinner display = {props.trendingList.loading} />
-        <CustomPagination  setPage = {setPage} />
+            <span className = 'page-heading'> 
+                <i className="fas fa-fire"></i>   
+                Trending
+            </span>
+            <div className = 'movie-container'>      
+                {props.trendingList.trendingListData.map(
+                    (content) => <Cards key = {content.id} {...content} />
+                )}
+            </div>  
+            <Spinner display = {props.trendingList.loading} />
+            <CustomPagination  setPage = {setPage} />
         </>
     )
 };
 
-const mapStateToProps = state => {
-    console.log(state);
+const mapStateToProps = state => { 
     return{
         trendingList : state.trendingList
     }
